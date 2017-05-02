34f4c4eb1686ed8d803c7acd338c2c2378bc669e
chris <langjiwt@gmail.com>
2017-05-03 00:34:54 +0800

    2017.05.03

--- app.js
+++ app.js
@@ -9,17 +9,74 @@ vm = new Vue({
         human:"",
         robots:"",
         items:[],
-        isHumanStep:true
+        isHumanStep:true,
+        selected:true,
+        steps:0,
+        results:[
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
     },
     methods:{
+        choose:function (e) {
+          this.human = e;
+          if(e == "O") {
+              this.robots = "X";
+          }
+          else{
+              this.robots = "O";
+          }
+          this.selected = false;
+        },
         step:function (e) {
             if(this.isHumanStep){
-
+                this.items[e.itemIndex].piece = this.human;
+                this.isHumanStep = false;
+                this.items[e.itemIndex].checked = true;
+                this.step();
             }
-            this.items[e.itemIndex].checked = true;
+            else{
+                if(this.steps === 0){
+                    var i = this.randomItemNumber();
+                    this.items[i].piece = this.robots;
+                    this.items[i].checked = true;
+                }
+                this.isHumanStep = true;
+            }
+
         },
         checkWin:function () {
+            let self = this;
+            let humanResult = [];
+            let robotResult = [];
 
+            this.items.reduce(function(acc,cur,index,arr){
+                if(item.piece == self.human){
+                    humanResult.push(item.itemIndex);
+                }
+                else{
+                    robotResult.push(item.itemIndex);
+                }
+            });
+        },
+        defence:function () {
+
+        },
+        positionLeft:function () {
+            var self = this;
+            return self.items.filter(function (e) {
+                return e.checked == false;
+            })
+        },
+        randomItemNumber:function () {
+            var itemNumber = this.positionLeft().length;
+            return this.positionLeft()[Math.floor(Math.random()*itemNumber)].itemIndex;
         }
     },
     created:function(){
