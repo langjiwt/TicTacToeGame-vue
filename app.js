ce729e169c2b7a60fb3e380e737df98510b3d5c0
Chris <langjiwt@gmail.com>
2017-05-03 14:14:35 +0800

    2017.05.03

--- app.js
+++ app.js
@@ -4,14 +4,11 @@
 vm = new Vue({
     el:'#app',
     data:{
-        circle:"O",
-        cross:"X",
         human:"",
         robots:"",
         items:[],
         isHumanStep:true,
-        selected:true,
-        steps:0,
+        selectDialog:true,
         results:[
             [0,1,2],
             [3,4,5],
@@ -25,58 +22,105 @@ vm = new Vue({
     },
     methods:{
         choose:function (e) {
-          this.human = e;
-          if(e == "O") {
-              this.robots = "X";
-          }
-          else{
-              this.robots = "O";
-          }
-          this.selected = false;
+            this.human = e;
+            this.robots = (e == "O")?"X":"O";
+            this.selectDialog = false;
         },
         step:function (e) {
             if(this.isHumanStep){
                 this.items[e.itemIndex].piece = this.human;
                 this.isHumanStep = false;
                 this.items[e.itemIndex].checked = true;
+                this.checkWin();
                 this.step();
             }
             else{
-                if(this.steps === 0){
+
+                let humanResult = this.humanIndexList();
+                let robotResult = this.robotsIndexList();
+                humanResult = this.matchNumber(humanResult);
+                robotResult = this.matchNumber(robotResult);
+                if((this.matchNumber(humanResult)[0] == 2)&&(this.items[humanResult[1]].checked == false)){
+                    this.items[humanResult[1]].piece = this.robots;
+                    this.items[humanResult[1]].checked = true;
+                }
+                else if((this.matchNumber(robotResult)[0] == 2)&&(this.items[robotResult[1]].checked == false)){
+                    this.items[robotResult[1]].piece = this.robots;
+                    this.items[robotResult[1]].checked = true;
+                }
+                else{
                     var i = this.randomItemNumber();
                     this.items[i].piece = this.robots;
                     this.items[i].checked = true;
+                    this.steps++;
                 }
+                this.checkWin();
                 this.isHumanStep = true;
             }
-
         },
+        //检查是否已取胜
         checkWin:function () {
-            let self = this;
-            let humanResult = [];
-            let robotResult = [];
-
-            this.items.reduce(function(acc,cur,index,arr){
-                if(item.piece == self.human){
-                    humanResult.push(item.itemIndex);
-                }
-                else{
-                    robotResult.push(item.itemIndex);
-                }
-            });
-        },
-        defence:function () {
-
+            let vm = this;
+            let humanResult = this.humanIndexList();
+            let robotResult = this.robotsIndexList();
+            humanResult = this.matchNumber(humanResult);
+            robotResult = this.matchNumber(robotResult);
+            //如果任意一方符合取胜结果数组中的数量为3个，则判断为已取胜
+            if(humanResult[0] == 3){
+                alert("human win");
+            }
+            if(robotResult[0] == 3){
+                alert("robots win");
+            }
         },
         positionLeft:function () {
-            var self = this;
+            let self = this;
             return self.items.filter(function (e) {
                 return e.checked == false;
             })
         },
         randomItemNumber:function () {
-            var itemNumber = this.positionLeft().length;
+            let itemNumber = this.positionLeft().length;
             return this.positionLeft()[Math.floor(Math.random()*itemNumber)].itemIndex;
+        },
+        humanIndexList:function () {
+            let vm = this;
+            let arr = [];
+            vm.items.forEach(function(item){
+                if(item.piece == vm.human){
+                    arr.push(item.itemIndex);
+                }
+            });
+            return arr;
+        },
+        robotsIndexList:function () {
+            let vm = this;
+            let arr = [];
+            vm.items.forEach(function(item){
+                if(item.piece == vm.robots){
+                    arr.push(item.itemIndex);
+                }
+            });
+            return arr;
+        },
+        matchNumber:function (arr) {
+            for(var i=0;i<this.results.length;i++) {
+                if ((arr.indexOf(this.results[i][0]) > -1) && (arr.indexOf(this.results[i][1]) > -1) && (arr.indexOf(this.results[i][2]) > -1)) {
+                    return [3, null];
+                }
+            }
+            for(var i=0;i<this.results.length;i++) {
+                    if((arr.indexOf(this.results[i][0])>-1)&&(arr.indexOf(this.results[i][1])>-1)){
+                        return [2,this.results[i][2]];
+                    }
+                    else if((arr.indexOf(this.results[i][1])>-1)&&(arr.indexOf(this.results[i][2])>-1)){
+                        return [2,this.results[i][0]];
+                    }
+                    else if((arr.indexOf(this.results[i][0])>-1)&&(arr.indexOf(this.results[i][2])>-1)){
+                        return [2,this.results[i][1]];
+                    }
+            }
+            return [1,null];
         }
     },
     created:function(){
@@ -85,7 +129,7 @@ vm = new Vue({
                 {
                     itemIndex:i,
                     checked:false,
-                    piece:'O'
+                    piece:' '
                 }
             );
         }
